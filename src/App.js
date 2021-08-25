558ca374e158108331f50af8422c2afe585673bc
Firas9717 <firas.sfaxi97@gmail.com>
2021-08-25 16:17:49 +0100

    first

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
   return (
     <div className="App">
       <Photo/>
-      <Fullname name="Gerald Earl Gillum (A.K.A_  G-easy) " />
+      <Fullname name=" G-easy) " />
       <Age age={31} />
       <Profession profession="Rappeur ,singer-songwriter and producer " />
       <Bio bio= "G-Eazy, real name Gerald Earl Gillum, born May 24, 1989 in Oakland, California, is an American rapper, singer-songwriter and producer. G-Eazy carried out numerous projects (mixtapes and EP) before giving birth to his first studio album. He describes his music as contemporary rap enriched with the style of 1950s culture." />
